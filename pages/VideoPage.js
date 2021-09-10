8e85b1f598c5695b73c42c33a58d41c1012a00cd
avinashsingh6641 <singhavinash6641@gmail.com>
2021-09-10 19:57:43 -0400

    video page added and player and upload button added

--- pages/VideoPage.js
+++ pages/VideoPage.js
@@ -1,10 +1,36 @@
 import classes from "./VideoPage.module.css";
+import React, { useState } from "react";
 
-const videoPage = () => {
+import ReactPlayer from "react-player";
+
+const VideoPage = () => {
+  const [enteredUrl, setEnteredUrl] = useState("");
+  const urlChangeHandler = (event) => {
+    setEnteredUrl(URL.createObjectURL(event.target.files[0]));
+    console.log(enteredUrl);
+  };
+
   return (
     <div className={classes.video}>
-      <p>video page</p>
+
+      <input
+        type="file"
+        id="file"
+        accepts="videos/*"
+        onChange={urlChangeHandler}
+      />
+      <label htmlFor="file">
+        <span className="material-icons">movie_creation</span>&nbsp;
+        select video
+      </label>
+      <ReactPlayer
+        className={classes.player}
+        url={enteredUrl}
+        width="50%"
+        height="50%"
+        controls={true}
+      />
     </div>
   );
 };
-export default videoPage;
+export default VideoPage;
